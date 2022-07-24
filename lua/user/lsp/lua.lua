e69c614b9de398e939e532ed844cc16f4daf5d64
Benjamin Hofmann <ben@bentop.fritz.box>
2022-07-24 22:25:39 +0200

    runtime_path not neeeded

--- lua/user/lsp/lua.lua
+++ lua/user/lsp/lua.lua
@@ -6,7 +6,6 @@ require'lspconfig'.sumneko_lua.setup {
             -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
             version = 'LuaJIT',
             -- Setup your lua path
-            path = runtime_path,
         },
         diagnostics = {
             -- Get the language server to recognize the `vim` global
