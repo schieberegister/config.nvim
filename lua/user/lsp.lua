5e33380c32747cba94d2a312c174bd9b83b4cebf
Benjamin Hofmann <benni@MacBook-Pro.fritz.box>
2023-05-13 11:06:18 +0200

    add bash lsp

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -31,7 +31,7 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
@@ -99,3 +99,4 @@ require('user.lsp.go')
 require('user.lsp.lua')
 require('user.lsp.python')
 require('user.lsp.c')
+require('user.lsp.bash')
