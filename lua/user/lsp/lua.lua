81e028e44d65e044c537e746a6a2cdab449257e7
Benjamin Hofmann <benni@MacBook-Pro.fritz.box>
2023-05-13 11:05:42 +0200

    exchage the lua lsp for a supported one

--- lua/user/lsp/lua.lua
+++ lua/user/lsp/lua.lua
@@ -1,24 +1,22 @@
-require'lspconfig'.sumneko_lua.setup {
-  cmd = {'lua-language-server'};
+require'lspconfig'.lua_ls.setup {
   settings = {
-      Lua = {
+    Lua = {
       runtime = {
-          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-          version = 'LuaJIT',
-          -- Setup your lua path
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
       },
       diagnostics = {
-          -- Get the language server to recognize the `vim` global
-          globals = {'vim'},
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
       },
       workspace = {
-          -- Make the server aware of Neovim runtime files
-          library = vim.api.nvim_get_runtime_file("", true),
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
       },
       -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = {
-          enable = false,
+        enable = false,
       },
-      },
+    },
   },
 }
